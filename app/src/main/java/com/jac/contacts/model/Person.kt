34308d7052c85e432a0bdcc2f42540e7e2a0ecf4
jacurban <jac.urban2@gmail.com>
2019-04-25 11:31:10 -0300

    parcelize issue

--- app/src/main/java/com/jac/contacts/model/Person.kt
+++ app/src/main/java/com/jac/contacts/model/Person.kt
@@ -4,17 +4,20 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.Ignore
 import android.arch.persistence.room.PrimaryKey
 import android.os.Parcelable
+import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 
 // tabela 'Person'
-@Entity(tableName = "person") @Parcelize
+
+@Entity(tableName = "person")   @Parcelize
 data class Person(
     @ColumnInfo(name = "name")
     var name: String,
     @ColumnInfo(name = "email") @PrimaryKey
     var email: String,
     @ColumnInfo(name = "phone")
-    val phone: String,
+    val phone: String
+) : Parcelable {
+    @IgnoredOnParcel
     @Ignore
-    var visible: Boolean = false
-) : Parcelable
+    var visible: Boolean = false}
